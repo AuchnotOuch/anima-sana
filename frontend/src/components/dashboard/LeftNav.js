d05bdb49cf9c76e085906630b359e3dc93dc0851
Alex Auch <alexsamuelauch@gmail.com>
2023-06-04 15:00:53 -0600

    added basic buttons to right and left nav areas on dashboard

--- frontend/src/components/dashboard/LeftNav.js
+++ frontend/src/components/dashboard/LeftNav.js
@@ -3,7 +3,17 @@ import './Dashboard.css'
 
 const LeftNav = () => {
     return (
-        <div className='left-nav'>left nav</div>
+        <div className='left-nav'>
+            <div className='home-button-container'>
+                <button className='home-button'>Home</button>
+            </div>
+            <div className='health-button-container'>
+                <button className='health-button'>Health</button>
+            </div>
+            <div className='journal-button-container'>
+                <button className='journal-button'>Journal</button>
+            </div>
+        </div>
     )
 }
 
--- frontend/src/components/dashboard/RightNav.js
+++ frontend/src/components/dashboard/RightNav.js
@@ -3,7 +3,17 @@ import './Dashboard.css'
 
 const RightNav = () => {
     return (
-        <div className='right-nav'>right nav</div>
+        <div className='right-nav'>
+            <div className='spaces-button-container'>
+                <button className='spaces-button'>Spaces</button>
+            </div>
+            <div className='friends-button-container'>
+                <button className='friends-button'>Friends</button>
+            </div>
+            <div className='settings-button-container'>
+                <button className='settings-button'>Settings</button>
+            </div>
+        </div>
     )
 }
 
